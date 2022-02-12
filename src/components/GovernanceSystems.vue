b14ba1e05d2836232a9a6fdc1a6291b1b232ffd2
Qing An <qingan88@hotmail.com>
2022-02-11 23:32:42 -0500

    add styles to Governance Systems

--- src/components/GovernanceSystems.vue
+++ src/components/GovernanceSystems.vue
@@ -1,14 +1,50 @@
 <template>
-  <div>
+  <div class="governance container">
     <h2>Governance Systems and Processes</h2>
-    <button type="button" class="btn btn-outline-secondary">
+    <button type="button" class="btn btn-outline-primary">
+      Reporting & Analytics
+    </button>
+    <button type="button" class="btn btn-outline-primary">
+      Reporting & Analytics
+    </button>
+    <button type="button" class="btn btn-outline-primary">
+      Reporting & Analytics
+    </button>
+    <button type="button" class="btn btn-outline-primary">
+      Reporting & Analytics
+    </button>
+    <button type="button" class="btn btn-outline-primary">
       Reporting & Analytics
     </button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {
+      resources: [{
+
+      }]
+    }
+  }
+};
 </script>
 
-<style></style>
+<style>
+.governance{
+  background-color:darkblue;
+  color: white;
+  padding: 1rem;
+  margin: 2rem auto;
+  border-radius: 2rem;
+  max-width: 40rem;
+}
+h2{
+  padding-bottom: 0.2em;
+}
+button.btn{
+  background-color: white;
+  margin:0 1em;
+}
+</style>
