2ad37c3fed29d212a6a64da406ea1b92ec6182db
Qing An <qingan88@hotmail.com>
2022-02-11 20:27:07 -0500

    add Governance component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GovernanceSystems msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GovernanceSystems from './components/GovernanceSystems.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GovernanceSystems
   }
 }
 </script>
---
+++ src/components/GovernanceSystems.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Governance Systems and Processes</h2>
+  <button type="button" class="btn btn-light">Reporting & Analytics</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
