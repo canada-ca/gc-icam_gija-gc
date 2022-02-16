3ed0b15e5d49f76d38124008212f0afd8fc091c3
Qing An <qingan88@hotmail.com>
2022-02-15 23:12:07 -0500

    modify main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BaseButton from './components/UI/BaseButton.vue'
+import BaseCard from './components/UI/BaseCard.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
+app.mount('#app')
