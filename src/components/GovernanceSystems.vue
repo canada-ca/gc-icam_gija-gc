f302c621e7310bc8e48c9284538d0f0c93a83394
Qing An <qingan88@hotmail.com>
2022-02-15 23:13:45 -0500

    update governance systems component for demo

--- src/components/GovernanceSystems.vue
+++ src/components/GovernanceSystems.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="governance container">
-    <h2>Governance Systems and Processes</h2>
-    <button type="button" class="btn btn-outline-primary">
+  <div class="governance container"  style="cursor:pointer">
+    <h2><a href="https://ancentury.github.io/ICAM/identityManagement-en.html" style="color:white">Governance Systems and Processes</a></h2>
+    <button type="button" class="btn example-popover" data-bs-trigger="hover focus" data-bs-toggle="popover" data-bs-content="the process of organizing data into informational summaries in order to monitor how different ICAM components are performing and complying with policy" title="Reporting & Analytics">
       Reporting & Analytics
     </button>
-    <button type="button" class="btn btn-outline-primary">
+    <button type="button" class="btn">
       Access Certification & Discovery
     </button>
-    <button type="button" class="btn btn-outline-primary">
+    <button type="button" class="btn">
       Approval Workflows
     </button>
-    <button type="button" class="btn btn-outline-primary">
+    <button type="button" class="btn">
       Reporting & Analytics
     </button>
-    <button type="button" class="btn btn-outline-primary">
+    <button type="button" class="btn">
       Reporting & Analytics
     </button>
   </div>
 </template>
 
 <script>
+import { Popover } from 'bootstrap/dist/js/bootstrap.esm.min.js'
 export default {
   data(){
     return {
@@ -27,6 +28,17 @@ export default {
 
       }]
     }
+  },
+  mounted(){
+    Array.from(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        .forEach(popoverNode => new Popover(popoverNode));
+  },
+  methods:{
+    governance(){
+    },
+    showDesc(){
+      
+    } 
   }
 };
 </script>
