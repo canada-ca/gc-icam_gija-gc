195b3f6adc2df697bdf2c92bdeb9c6075dbb4ac8
Qing An <qingan88@hotmail.com>
2022-02-18 20:31:18 -0500

    add resource consumers

--- src/components/ResourceConsumersCard.vue
+++ src/components/ResourceConsumersCard.vue
@@ -1,57 +1,45 @@
 <template>
   <div class="resourceConsumers">
-    <card-title>Resource <br>Consumers</card-title>
-    <popover-button v-for="accessManagement in accessManagements" :description="accessManagement.description" :title="accessManagement.title" :key="accessManagement.id">
-      {{accessManagement.title}}
+    <h4>Resource <br>Consumers</h4>
+    <popover-button v-for="resourceConsumer in resourceConsumers" :description="resourceConsumer.description" :title="resourceConsumer.title" :key="resourceConsumer.id">
+      {{resourceConsumer.title}}
     </popover-button>
   </div>
 </template>
 
 <script>
 import PopoverButton from './UI/PopoverButton.vue'
-import CardTitle from './UI/BaseCardTitle.vue'
 export default {
   components: {
     PopoverButton,
-    CardTitle
   },
   data(){
     return {
-      accessManagements:[
+      resourceConsumers:[
         {
-          id:"authentication",
-          title:"Authentication",
-          description:"verification of the identity of a user, process or device, often as a prerequisite to allowing access to resources in an information system"
+          id:"users",
+          title:"Users",
+          description:"internal and external consumers of GC system and information resources"
         },
         {
-          id:"authorization",
-          title:"Authorization",
-          description:"the process of determining whether or not an authenticated entity has permission to access a particular resource"
+          id:"businesses-and",
+          title:"Businesses and Organizations",
+          description:"businesses and other levels of government that interact with the GC"
         },
         {
-          id:"privilege-access",
-          title:"Privilege Access Management",
-          description:"a specialized area of ICAM that deals specifically with the protection of privileged accounts"
+          id:"devices",
+          title:"Devices",
+          description:"any device that needs to be authenticated to gain access to GC system and information resources"
         },
         {
-          id:"policy-repository",
-          title:"Policy Repository",
-          description:"a data repository or system that stores the rules/policies that define which attributes a requester must have in order to access a particular resource"
+          id:"applications",
+          title:"Applications",
+          description:"applications and services that interact with one another"
         },
         {
-          id:"physical-and",
-          title:"Physical and Logical Access Control Systems",
-          description:"a Physical Access Control System deals with controlling access to physical facilities and a Logical Access Control system controls an entity’s ability to access system and information resources"
-        },
-        {
-          id:"consent-management",
-          title:"Consent Management",
-          description:"provides users control over how their personal information is collected, used, retained, and disclosed"
-        },
-        {
-          id:"e-signature",
-          title:"e-Signature",
-          description:"an electronic representation of a individual’s signature that links that person to an electronic document or transaction and provides evidence of that person’s intent to approve or to be legally bound by its contents"
+          id:"ai-robots",
+          title:"AI/Robots",
+          description:"advanced technologies that can act on behalf of a user or embedded in a device or application"
         },
       ]
     }    
@@ -62,5 +50,8 @@ export default {
 <style scoped>
 .resourceConsumers {
   background-color: #fff;
+}
+.resourceConsumers h4{
+  color: black;
 }
 </style>
