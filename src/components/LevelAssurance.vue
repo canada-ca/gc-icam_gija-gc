d7fc318b7844f6d8d0eab6b464faaa09f202fc4e
Qing An <qingan88@hotmail.com>
2022-02-22 21:34:54 -0500

    modify levels of Assurance

--- src/components/LevelAssurance.vue
+++ src/components/LevelAssurance.vue
@@ -1,9 +1,9 @@
 <template>
   <img src="/assets/arrowUp.png" alt="Arrow" style="margin-top: -1rem; height: 5rem" />
   <div class="row">
-    <img src="/assets/arrowLeft.png" class="g-0" alt="Left Arrow" style="margin-left:-2rem; width:5rem" />
+    <img src="/assets/arrowLeft.png" class="col g-0" alt="Left Arrow" style="margin-left:-2rem; width:4rem" />
     <div id="levels" class="col g-0">Levels of Assurance</div>
-    <img src="/assets/arrowRight.png" class="g-0" alt="Right Arrow" style="margin-right:-2rem;width: 5rem" />
+    <img src="/assets/arrowRight.png" class="col g-0" alt="Right Arrow" style="margin-right:-2rem;width: 4rem" />
   </div>  
   <img src="/assets/arrowDown.png" alt="Arrow" style="margin-bottom: -1rem; height: 5rem" />
 </template>
