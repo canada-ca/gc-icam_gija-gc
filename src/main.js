6000d57e2c1c21a747de3cbbb969886446c7a867
Qing An <qingan88@hotmail.com>
2022-02-24 17:25:45 -0500

    fix github pages

--- src/main.js
+++ src/main.js
@@ -8,14 +8,14 @@ import BaseButton from './components/UI/BaseButton.vue'
 import BaseCard from './components/UI/BaseCard.vue'
 import GovernanceSystems from './components/GovernanceSystems.vue'
 import IdentityManagement from './components/IdentityManagement.vue'
-import Home from  './components/Home.vue'
+import Home from './components/Home.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 library.add(fas)
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/gc-icam_gijia-gc/'),
   routes: [
     {
       path: '/',
