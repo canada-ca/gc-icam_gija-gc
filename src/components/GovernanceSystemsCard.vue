116f59e139d9b9a3442422f3fa28cb0796edeb66
Qing An <qingan88@hotmail.com>
2022-03-03 21:27:43 -0500

    centralize governance systems

--- src/components/GovernanceSystemsCard.vue
+++ src/components/GovernanceSystemsCard.vue
@@ -26,48 +26,20 @@
 import BaseCard from "./UI/BaseCard.vue";
 import BaseCardTitle from "./UI/BaseCardTitle.vue";
 import PopoverButton from "./UI/PopoverButton.vue";
+import governanceSystems from "../resources/governanceSystems.json";
+
 export default {
   components: {
     PopoverButton,
     BaseCardTitle,
-    BaseCard,
+    BaseCard
   },
   data() {
     return {
-      governanceSystems: [
-        {
-          id: "reporting-analytics",
-          title: "Reporting & Analytics",
-          description:
-            "the process of organizing data into informational summaries in order to monitor how different ICAM components are performing and complying with policy",
-        },
-        {
-          id: "access-certification",
-          title: "Access Certification & Discovery",
-          description: "the process of validating access rights within systems",
-        },
-        {
-          id: "approval-workflows",
-          title: "Approval Workflows",
-          description:
-            "processes used to manage the life cycle of a digital identity including provisioning, on-going maintenance and de-provisioning of identity attributes",
-        },
-        {
-          id: "policy-compliance",
-          title: "Policy and Compliance",
-          description:
-            "processes used to ensure compliance to applicable organizational policies and legislation",
-        },
-        {
-          id: "monitoring-auditing",
-          title: "Monitoring, Auditing and Logging",
-          description:
-            "processes and tools used to support ongoing evaluation and assessment that various information systems are operating properly and are being used as intended",
-        },
-      ],
+      governanceSystems
     };
   },
-  methods: {},
+  methods: {}
 };
 </script>
 
