dbba2764f158d805ae4b53fc4ea223d0c3dc2467
Qing An <qingan88@hotmail.com>
2022-03-03 22:53:41 -0500

    centralize identitymanagement

--- src/components/IdentityManagementCard.vue
+++ src/components/IdentityManagementCard.vue
@@ -20,59 +20,18 @@
 import BaseCard from "./UI/BaseCard.vue";
 import CardTitle from "./UI/BaseCardTitle.vue";
 import PopoverButton from "./UI/PopoverButton.vue";
+import identityManagements from "../resources/identityManagement.json";
 export default {
   components: {
     BaseCard,
     PopoverButton,
-    CardTitle,
+    CardTitle
   },
   data() {
     return {
-      identityManagements: [
-        {
-          id: "identity-establishment",
-          title: "Identity Establishment",
-          description:
-            "the creation of an authoritative record of identity that is relied on by others for subsequent government activities",
-        },
-        {
-          id: "entitlements-and",
-          title: "Entitlements and Roles",
-          description:
-            "the set of privileges or entitlements associated with an entity that support informed access control decisions",
-        },
-        {
-          id: "identity-lifecycle",
-          title: "Identity Lifecycle Management",
-          description:
-            "the provisioning, ongoing maintenance and de-provisioning of identity attributes",
-        },
-        {
-          id: "authoritative-attribute",
-          title: "Authoritative Attribute Exchange Service",
-          description:
-            "a broker between multiple, disparate Authoritative Sources and the consumers of the identity information",
-        },
-        {
-          id: "identity-repositories",
-          title: "Identity Repositories",
-          description: "any data repository that stores identity information",
-        },
-        {
-          id: "delegated-administration",
-          title: "Delegated Administration",
-          description:
-            "the de-centralization of certain system administration functions",
-        },
-        {
-          id: "user-self",
-          title: "User Self-Service",
-          description:
-            "the ability to allow users to specify or change certain identity attributes",
-        },
-      ],
+      identityManagements
     };
-  },
+  }
 };
 </script>
 
