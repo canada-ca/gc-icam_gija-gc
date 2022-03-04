becebd3405418f09d1582f1625accd4c3e608dff
Qing An <qingan88@hotmail.com>
2022-03-03 22:54:24 -0500

    contralize identity proofing

--- src/components/IdentityProofingCard.vue
+++ src/components/IdentityProofingCard.vue
@@ -1,49 +1,46 @@
 <template>
   <vertical-card class="identityProofing">
-    <popover-button :id="identityProofing.id" v-for="identityProofing in identityProofings" :description="identityProofing.description" :title="identityProofing.title" :key="identityProofing.id">
-      {{identityProofing.title}}
+    <popover-button
+      v-for="identityProofing in identityProofings"
+      :id="identityProofing.id"
+      :description="identityProofing.description"
+      :title="identityProofing.title"
+      :key="identityProofing.id"
+    >
+      {{ identityProofing.title }}
     </popover-button>
   </vertical-card>
 </template>
 
 <script>
-import VerticalCard from './UI/VerticalCard.vue'
-import PopoverButton from './UI/PopoverButton.vue'
+import VerticalCard from "./UI/VerticalCard.vue";
+import PopoverButton from "./UI/PopoverButton.vue";
+import identityProofing from "../resources/identityProofing.json";
+
 export default {
   components: {
     VerticalCard,
-    PopoverButton,
+    PopoverButton
   },
-  data(){
+  data() {
     return {
-      identityProofings:[
-        {
-          id:"identity-proofing",
-          title:"Identity Proofing",
-          description:"the function of collecting evidence [identity attributes] which supports a claim of identity [for a specific entity] and the validation and verification of that evidence so as to determine the veracity (or otherwise) of the claim"
-        },
-        {
-          id:"sponsorship",
-          title:"Sponsorship",
-          description:"to formally establish that an organization or Non-Person Entities (NPE) entity requires access to GC resources"
-        },
-      ]
-    }    
+      identityProofings: identityProofing
+    };
   }
-}
+};
 </script>
 
 <style scoped>
 .identityProofing {
   background-color: #c00000;
 }
-#identity-proofing{
+#identity-proofing {
   width: 12rem;
   margin: 12rem 0.5rem 2rem;
   transform-origin: 0 0;
   transform: rotate(-90deg);
 }
-#sponsorship{
+#sponsorship {
   width: 30rem;
   margin: 27rem 0.5rem 2rem;
   transform-origin: 0 0;
