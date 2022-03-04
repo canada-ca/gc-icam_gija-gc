941ffbabd43756a2cc492f9b3267583c5eda88e3
Qing An <qingan88@hotmail.com>
2022-03-03 21:11:44 -0500

    centralize authoritative sources

--- src/components/AuthoritativeSourcesCard.vue
+++ src/components/AuthoritativeSourcesCard.vue
@@ -25,54 +25,18 @@
 import BaseCard from "./UI/BaseCard.vue";
 import PopoverButton from "./UI/PopoverButton.vue";
 import CardTitle from "./UI/BaseCardTitle.vue";
+import authoritativeSources from "../resources/authoritativeSources.json";
 export default {
   components: {
     BaseCard,
     PopoverButton,
-    CardTitle,
+    CardTitle
   },
   data() {
     return {
-      authoritativeSources: [
-        {
-          id: "directories-inventory",
-          title: "Directories & Inventory Systems",
-          description:
-            "systems or data repositories that store information about users and NPEs",
-        },
-        {
-          id: "hr-pay",
-          title: "HR, Pay & Screening Systems",
-          description:
-            "sources of certain identity information associated with internal users",
-        },
-        {
-          id: "international-sources",
-          title: "International Sources",
-          description:
-            "sources of identity information for non-Canadian citizens",
-        },
-        {
-          id: "other-levels",
-          title: "Other Levels of Government",
-          description:
-            "sources of information for internal user, external users and businesses",
-        },
-        {
-          id: "industry-sources",
-          title: "Industry Sources",
-          description:
-            "sources of information for external users and businesses",
-        },
-        {
-          id: "other-sources",
-          title: "Other Sources",
-          description:
-            "sources of information for external users and businesses",
-        },
-      ],
+      authoritativeSources: authoritativeSources
     };
-  },
+  }
 };
 </script>
 
