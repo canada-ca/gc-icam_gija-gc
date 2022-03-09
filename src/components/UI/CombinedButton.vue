98df3439eac93413a3a7abf6d09048b0a316c145
Qing An <qingan88@hotmail.com>
2022-03-08 19:48:24 -0500

    create combined button

---
+++ src/components/UI/CombinedButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <a type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"
+    ><popover-button
+      :description="buttonInfo.description"
+      :title="buttonInfo.title"
+      >{{ buttonInfo.title }}</popover-button
+    ></a
+  >
+</template>
+
+<script>
+import PopoverButton from "./PopoverButton.vue";
+export default {
+  props: ["buttonInfo"],
+  components: {
+    PopoverButton
+  }
+};
+</script>
