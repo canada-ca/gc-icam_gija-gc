5e7d1348ed58a5e999e3db6f5aa12707b9a4e86e
Qing An <qingan88@hotmail.com>
2022-03-10 23:27:53 -0500

    make modal larger

--- src/components/UI/ModalCard.vue
+++ src/components/UI/ModalCard.vue
@@ -6,7 +6,7 @@
     aria-labelledby="exampleModalLabel"
     aria-hidden="true"
   >
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">
@@ -27,7 +27,7 @@
 
 <script>
 export default {
-  props: ["modalHead", "modalBody"],
+  props: ["modalHead", "modalBody"]
 };
 </script>
 
