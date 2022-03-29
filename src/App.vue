78d2081bae92860baf9d326218bc3b72a68f32e1
Qing An <qingan88@hotmail.com>
2022-03-28 23:06:27 -0400

    modify App to remember locale

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,14 @@ export default {
     const locale = localStorage.getItem("locale");
     if (locale) {
       this.$i18n.locale = locale;
+    } else if (navigator.language) {
+      this.$i18n.locale = navigator.language.substring(0, 2);
     }
+    this.$router.push({
+      params: {
+        lang: this.$i18n.locale
+      }
+    });
   }
 };
 </script>
