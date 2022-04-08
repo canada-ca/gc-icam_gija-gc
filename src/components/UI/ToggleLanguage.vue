1d8d2f58da521884f184eb2665f3308fe6f48092
Qing An <qingan88@hotmail.com>
2022-04-07 22:59:53 -0400

    create toggle language component

---
+++ src/components/UI/ToggleLanguage.vue
@@ -0,0 +1,31 @@
+<template>
+  <a
+    href="#"
+    title="Français - Version française de cette page"
+    lang="fr"
+    @click="toggleLang"
+    >{{ computedLang }}</a
+  >
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleLang() {
+      if (this.$i18n.locale == "fr") {
+        this.$i18n.locale = "en";
+      } else {
+        this.$i18n.locale = "fr";
+      }
+      let locale = this.$i18n.locale;
+      localStorage.setItem("locale", locale);
+      this.$router.push({ params: { lang: locale } });
+    }
+  },
+  computed: {
+    computedLang() {
+      return this.$i18n.locale == "fr" ? "English" : "Français";
+    }
+  }
+};
+</script>
